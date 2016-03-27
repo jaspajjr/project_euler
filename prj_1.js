3d731206965e41097ac73f1a12620bf4e37e4ea0
John Carney <jaspajr@gmail.com>
2016-03-27 20:26:29 +0100

    Minor amendments

---
+++ prj_1.js
@@ -0,0 +1,36 @@
+// find the sum of all the multiples of 3 or 5 below 1000
+//
+var upperLim = 10
+
+// to do this we need to iterate over every integer between 0 and 100, and check
+// if it is a multiple of 3 or 5
+
+function sumMultiplesN(N){
+  var numsToCheck = [3, 5];  
+  var multipleArray = numsToCheck.reduce(function(all, item, index) {
+    // this assumes that cos I think that it's in a closure it should know the 
+    // value of N
+    // console.log('N:', N);
+    // check if every item between 0 and N is a multiple of the item
+    // for the moment I will do this by using a loop as i can iterate that 
+    // between 0 and N, once I can create an array approprately, then I can 
+    // use reduce rather than a loop
+    for(i = 0; i < N; i++){
+      if(i % item == 0){
+        // if it is an item then addition
+        all.push(i);
+      };
+    }
+    // make sure that values are unique
+  return all;
+  }, []);
+  // now that we have an array of unique multiples lower than N we just need to
+  // sum them
+  console.log(multipleArray)
+  answer = multipleArray.reduce((a, b) => a + b, 0);
+  return answer
+}
+
+// Call the function 
+var answer =  sumMultiplesN(upperLim)
+console.log(answer)
